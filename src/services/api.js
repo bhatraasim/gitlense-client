0278e7eb716d8d26caa0ceb9a5bf46277bb63b55
rasim <rasim44440@gmail.com>
2026-03-02 14:11:50 +0530

    changed the api login req

--- src/services/api.js
+++ src/services/api.js
@@ -20,7 +20,7 @@ export const login = async (email, password) => {
   const response = await fetch(`${API_BASE_URL}/auth/login`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ email, password })
+    body: JSON.stringify({ name: "login_user", email, password })
   });
   const data = await response.json();
   
